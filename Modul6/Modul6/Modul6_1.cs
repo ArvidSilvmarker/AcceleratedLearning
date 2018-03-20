a8aec9bb781a9fad5b5e2fcc0a9bd04bf024b197
Arvid Silvmarker <arvid.silvmarker@gmail.com>
2018-03-20 10:07:02 +0100

    Modul 6.1 med cirklar och sånt.

---
+++ Modul6/Modul6/Modul6_1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul6
+{
+    class Modul6_1
+    {
+        public void Run()
+        {
+            Circle bob = new Circle("Bob", 8);
+            Sphere lisa = new Sphere("Lisa", 8);
+            HyperSphere ali = new HyperSphere("Ali", 8);
+
+            bob.SayHello();
+            lisa.SayHello();
+            ali.SayHello();
+
+            Console.WriteLine();
+
+            bob.WriteArea();
+            lisa.WriteArea();
+            ali.WriteArea();
+        }
+    }
+
+    class Circle
+    {
+        public string Name { get; set; }
+        public double Radius { get; set; }
+        public Circle(string name, double radius)
+        {
+            Name = name;
+            Radius = radius;
+        }
+
+        public Circle(string name)
+        {
+            Name = name;
+            Radius = 5;
+        }
+
+        public Circle()
+        {
+            Name = "Unknown";
+            Radius = 5;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"I'm a circle called {Name}");
+        }
+
+        public void WriteArea()
+        {
+            double area = Math.Round(Radius * Radius * Math.PI, 2); 
+            Console.WriteLine($"I'm {Name} the circle and I have a radius of {Radius} and my area is {area}.");
+        }
+    }
+
+    class Sphere : Circle
+    {
+        public Sphere(string name, double radius) : base(name, radius) { }
+        public Sphere(string name) : base(name) { }
+        public Sphere() : base() { }
+
+        public new void WriteArea()
+        {
+            double area = Math.Round(4 * Radius * Radius * Math.PI, 2);
+            Console.WriteLine($"I'm {Name} the sphere and I have a radius of {Radius} and my area is {area}.");
+        }
+    }
+
+    class HyperSphere : Sphere
+    {
+        public HyperSphere(string name, double radius) : base(name, radius) { }
+
+        public new void WriteArea()
+        {
+            double area = Math.Round(2 * Math.Pow(Radius, 3) * Math.Pow(Math.PI, 2), 2);
+            Console.WriteLine($"I'm {Name} the hypersphere and I have a 4-dimensional radius of {Radius} and my hyperarea is {area}.");
+        }
+
+    }
+
+}
---
+++ Modul6/Modul6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Modul6
+{
+    class BlogPost
+    {
+        public string Header { get; set; }
+        public DateTime PostedDate { get; set; }
+
+        public BlogPost(string header)
+        {
+            Header = header;
+            PostedDate = DateTime.Now;
+        }
+        public BlogPost(string header, DateTime postedDate)
+        {
+            Header = header;
+            PostedDate = postedDate;
+        }
+
+        public void WriteHeader()
+        {
+            Console.WriteLine($"Bloggpostens har rubrik {Header}, och postades {PostedDate}");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var b1 = new BlogPost("Min rubrik", new DateTime(2018,03,19));
+            //var b2 = new BlogPost("Min andra rubrik");
+            //b1.WriteHeader();
+            //b2.WriteHeader();
+            var modul = new Modul6_1();
+            modul.Run();
+        }
+    }
+}
