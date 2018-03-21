3017e959e3d0fddd3d3e2aecdf5e5c0ff844313f
Arvid Silvmarker <arvid.silvmarker@gmail.com>
2018-03-21 14:03:19 +0100

    8.1 (exceptions och chokladdelning)

---
+++ Modul8/Modul8/Modul8_1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Modul8
+{
+    class Modul8_1
+    {
+        public void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    ChokolateDivider cd = new ChokolateDivider(24);
+                    Console.WriteLine($"Antal choklatbitar att dela: {cd.PiecesOfChokolate}");
+                    Console.Write("Antal personer: ");
+                    int people = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Varje person får: {cd.Divide(people):0.00}");
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Message);
+                    Console.ResetColor();
+                    continue;
+                }
+            }
+
+        }
+    }
+
+    class ChokolateDivider
+    {
+        public int PiecesOfChokolate { get; set; }
+
+        public ChokolateDivider(int pieces)
+        {
+            PiecesOfChokolate = pieces;
+        }
+
+        public decimal Divide(int people)
+        {
+            if (people == 0) throw new DivideByZeroException("Noll personer kan inte dela på choklad.");
+            if (people == 666) throw new DevilException();
+            return (PiecesOfChokolate / (decimal)people);
+        }
+    }
+
+    class DevilException : Exception
+    {
+        public DevilException(string message) : base(message)
+        {
+        }
+
+        public DevilException() : base("Cookies of the Beast.")
+        {
+        }
+    }
+}
---
+++ Modul8/Modul8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Modul8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new Modul8_1();
+            app.Run();
+        }
+    }
+}
