5a109669db8af73a658d3c7ec01a79427612aabb
Arvid Silvmarker <arvid.silvmarker@gmail.com>
2018-03-23 13:05:41 +0100

    9.1 delegates

---
+++ Modul9/Modul9/Modul9_1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Modul9
+{
+    public class Modul9_1
+    {
+        public void Run()
+        {
+            string question = "Enter a string to convert: ";
+            GetInputAndConvert(question, ToUpper);
+            GetInputAndConvert(question, TrippleText);
+            GetInputAndConvert(question, AddStars);
+
+
+        }
+
+        private void GetInputAndConvert(string text, Func<string, string> Convert)
+        {
+            Console.Write(text);
+            Console.WriteLine($"Here is the result: {Convert(Console.ReadLine())}");
+            Console.WriteLine();
+        }
+
+        private string ToUpper(string text)
+        {
+            return text.ToUpper();
+        }
+
+        private string AddStars(string input)
+        {
+            List<char> output = new List<char>();
+            foreach (var ch in input)
+            {
+                output.Add(ch);
+                output.Add('*');
+            }
+
+            output.RemoveAt(output.Count-1);
+
+            string text = "";
+            foreach (char ch in output)
+                text += ch;
+
+            return text;
+        }
+
+        private string RepeatText(string input, int count)
+        {
+            string output = "";
+            for (int i = 0; i < count; i++)
+                output += input;
+            return output;
+        }
+
+        private string TrippleText(string input)
+        {
+            return RepeatText(input, 3);
+        }
+    }
+}
---
+++ Modul9/Modul9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Modul9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new Modul9_1();
+            app.Run();
+        }
+    }
+}
